cc94f2d6d7b64c3a92e9475b3d85908bf3896de2
Simon Norman <01secn@gmail.com>
2019-04-08 12:41:30 +0100

    All functionality added

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,8 +1,18 @@
 
-const fizzbuzz = (no: number): string => {
+const fizzbuzz = (no: number): any => {
   if(no % 3 === 0 && no % 5 === 0) {
     return 'fizzbuzz'
   }
+
+  if(no % 3 == 0) {
+    return 'fizz'
+  }
+
+  if(no % 5 == 0) {
+    return 'buzz'
+  }
+
+  return no
 }
 
 export default fizzbuzz;
--- test/fizzbuzz_test.ts
+++ test/fizzbuzz_test.ts
@@ -5,4 +5,16 @@ describe('Fizzbuzz', function() {
   it('should return fizzbuzz when number is divisible by 3 and 5', function () {
     expect(fizzbuzz(15)).equals('fizzbuzz');
   })
+
+  it('should return fizz when number is divisible by 3 only', function () {
+    expect(fizzbuzz(6)).equals('fizz');
+  })
+
+  it('should return buzz when number is divisible by 5 only', function () {
+    expect(fizzbuzz(10)).equals('buzz');
+  })
+
+  it('should return number when not divisible by 3 or 5', function () {
+    expect(fizzbuzz(17)).equals(17);
+  })
 })
